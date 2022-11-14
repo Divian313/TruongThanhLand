5129cc6e171fe2229c56e5fdcc1fc5c43600d1ac
vanhhoasen313 <ANH.DV0389@sinhvien.hoasen.edu.vn>
2022-11-14 22:24:30 +0700

    update

--- truongthanhland/src/components/Header/Header.jsx
+++ truongthanhland/src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import styled from "styled-components";
 import logo from "./image/logo-truongthanh.png";
 const WrapHeader = styled.div`
@@ -8,14 +9,12 @@ const WrapHeader = styled.div`
     margin: 0;
     display: flex;
     justify-content: center;
-    list-style-type: none;
-    
+    list-style: none;
   }
-  
+
   a {
     display: block;
-    font-family: 'Open Sans', sans-serif;
-
+    font-family: "Open Sans", sans-serif;
 
     font-size: 1.1em;
     font-weight: 500;
@@ -23,22 +22,39 @@ const WrapHeader = styled.div`
     padding: 0 12px;
     text-decoration: none;
     white-space: nowrap;
-}
-`
+  }
+`;
 const ListDuAn = styled.div`
   width: 270px;
   height: auto;
-  
-  /* display: flex; */
-  /* flex-direction: column; */
+  transform: translate(5%, 30%);
+  /* display: ${(props) => (props.chuotXuatHien && "block") || "none"}; */
+  display: block;
   background-color: red;
   position: absolute;
   a {
-    padding: 5px 10px;
+    display: inline-block;
+    margin-left: 0px;
+    padding: 8px 10px;
   }
-`
+  li:before {
+    content: "\f105";
+    font-family: FontAwesome;
+    display: inline;
+    margin-left: 10px;
+    width: 1.3em;
+    z-index: 9;
+  }
+`;
 
 const Header = () => {
+  const [chuotVaoDuAn, setChuotVaoDuAn] = useState(false);
+  const mouseMoveHandler = () => {
+    setChuotVaoDuAn(true);
+  };
+  const mouseOutHandler = () => {
+    setChuotVaoDuAn(false);
+  };
   return (
     <div className="container-fluid">
       <WrapHeader className="row text-center">
@@ -51,13 +67,23 @@ const Header = () => {
               <a href="">TRANG CHỦ</a>
             </li>
             <li>
-              <a href="">DỰ ÁN</a>
-              <ListDuAn className="text-start">
-                
-                  <li><a href="">Vinhomes</a></li>
-                  <li><a href="">Zeit River</a></li>
-                  <li><a href="">Zeit River</a></li>
-                
+              <a
+                onMouseOver={mouseMoveHandler}
+                onMouseOut={mouseOutHandler}
+                href=""
+              >
+                DỰ ÁN
+              </a>
+              <ListDuAn chuotXuatHien={chuotVaoDuAn} className="text-start">
+                <li>
+                  <a href="">Vinhomes</a>
+                </li>
+                <li>
+                  <a href="">Zeit River</a>
+                </li>
+                <li>
+                  <a href="">Zeit River</a>
+                </li>
               </ListDuAn>
             </li>
             <li>
