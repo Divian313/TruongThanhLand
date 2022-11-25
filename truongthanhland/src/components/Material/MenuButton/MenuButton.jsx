38c76f068378194bc37507e594fd698c6f43e9b1
vanhhoasen313 <ANH.DV0389@sinhvien.hoasen.edu.vn>
2022-11-25 19:18:36 +0700

    update

--- truongthanhland/src/components/Material/MenuButton/MenuButton.jsx
+++ truongthanhland/src/components/Material/MenuButton/MenuButton.jsx
@@ -1,14 +1,54 @@
 import styled from "styled-components";
+import Header from "../../Header/Header";
 const Wrap = styled.div`
-    /* width: 200px;
+  /* width: 200px;
     height: 50px;
     background-color: red; */
+`;
+const Button = styled.div`
+   width: 1.3rem;
+    cursor: pointer;
+    margin-right: 1em;
 `
 const MenuButton = () => {
-    return (
-        <Wrap className="btn btn-primary d-xl-none d-block">
-            menu
-        </Wrap>
-    )
-}
+  return (
+    <Wrap className="d-xl-none d-block">
+      <Button
+        // className="btn btn-primary"
+        // type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#offcanvasRight"
+        aria-controls="offcanvasRight"
+      >
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+        
+      </Button>
+
+      <div
+        class="offcanvas offcanvas-end"
+        tabindex="-1"
+        id="offcanvasRight"
+        aria-labelledby="offcanvasRightLabel"
+      >
+        <div class="offcanvas-header">
+          <h5 class="offcanvas-title" id="offcanvasRightLabel">
+            Offcanvas right
+          </h5>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
+        </div>
+        <div class="offcanvas-body">
+            {/* <Header></Header> */}
+            
+            </div>
+      </div>
+    </Wrap>
+  );
+};
 export default MenuButton;
+
+
