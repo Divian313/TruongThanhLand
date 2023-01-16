e2546bf83304cfe52037783292eaa9bf5a070464
vanhhoasen313 <ANH.DV0389@sinhvien.hoasen.edu.vn>
2023-01-16 22:51:16 +0700

    update

--- truongthanhland/src/components/Contents/Footer/Footer.jsx
+++ truongthanhland/src/components/Contents/Footer/Footer.jsx
@@ -1,5 +1,18 @@
 import IMGlogo from "./image/logo-truongthanh.png";
+
 import styled from "styled-components";
+import IconPhone from "./Material/IconPhone";
+import IconZalo from "./Material/IconZalo";
+
+const Icon = styled.div`
+  width: 50px;
+  height: 200px;
+  /* background-color: #dfd2d2; */
+  z-index: 99;
+  bottom: 50px;
+  right: 10px;
+`;
+
 const Hotline = styled.div`
   width: 100%;
   background: linear-gradient(45deg, #d07d0b, #ffc913);
@@ -19,26 +32,38 @@ const P = styled.p`
   font-weight: 700 !important;
 `;
 const H5 = styled.h5`
-  font-weight: 600;
+  font-weight: 500;
   margin-bottom: 15px;
   font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
-
 `;
 const Wrap = styled.div`
   font-family: "Crimson Text", serif;
   font-weight: 400;
+  img {
+    width: 100%;
+  }
 `;
 const UL = styled.ul`
   li {
   }
 `;
 const WrapInfo = styled.div`
+  font-family: "Crimson Text", serif;
+  white-space: nowrap;
+  a {
+    font-size: 1.1rem;
+    color: black;
+    text-decoration: none;
+  }
+  i {
+    margin: 5px 10px;
+    font-size: 2.5em;
+  }
+`;
 
-  
-`
 const Footer = () => {
   return (
-    <div className="w-100 mt-5">
+    <div className="w-100 mt-5 position-relative">
       <Hotline className="text-center">
         <a href="tel:0901563356">Hotline: 0901 563 356</a>
       </Hotline>
@@ -47,7 +72,7 @@ const Footer = () => {
           <img src={IMGlogo} alt="" srcset="" />
         </div>
         <div className="col-7">
-          <h5 style={{ color: "red", fontWeight: 1000, marginBottom:20 }}>
+          <h5 style={{ color: "red", fontWeight: 1000, marginBottom: 20 }}>
             CÔNG TY TNHH DỊCH VỤ BẤT ĐỘNG SẢN TRƯỜNG THÀNH
           </h5>
           <UL>
@@ -67,13 +92,30 @@ const Footer = () => {
         </div>
         <WrapInfo className="col-3">
           <H5>GIỚI THIỆU</H5>
-          <li className="mb-3"><a href="#">Thông tin công ty</a></li>
+          <li className="mb-3">
+            <a href="#">Thông tin công ty</a>
+          </li>
           <H5>THÔNG TIN</H5>
-          <li><a href="#">Chế độ bảo hành</a></li>
-          <li><a href="#">Chính sách bảo mật</a></li>
-          <li><a href="#">Điều khoản sử dụng</a></li>
+          <li>
+            <a href="#">Chế độ bảo hành</a>
+          </li>
+          <li>
+            <a href="#">Chính sách bảo mật</a>
+          </li>
+          <li>
+            <a href="#">Điều khoản sử dụng</a>
+          </li>
+          <a href="">
+            <i class="fa fa-facebook-official" aria-hidden="true"></i>
+          </a>
+          <i class="fa fa-youtube-square" aria-hidden="true"></i>
         </WrapInfo>
       </Wrap>
+      <Icon className="Icon position-fixed ">
+        <a href="tel:0901563356"><IconPhone></IconPhone></a>
+        <a href="zalo://chat?id="><IconZalo></IconZalo></a>
+      </Icon>
+      
     </div>
   );
 };
---
+++ truongthanhland/src/components/Contents/Footer/Material/IconPhone.jsx
@@ -0,0 +1,49 @@
+import IMGphone from "../image/phone-svgrepo-com.png";
+
+import styled from "styled-components";
+const Wrap = styled.div`
+  /* animation: zoom 0.5s ease-in-out infinite , shake 0.5s ease-in-out infinite; */
+  animation: zoom 0.5s ease-in-out infinite;
+  transform-origin: center;
+  @keyframes zoom {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(1.1);
+    }
+  }
+`;
+const IMG = styled.div`
+  margin-bottom: 10px;
+  animation: shake 0.5s ease-in-out infinite;
+  @keyframes shake {
+    0% {
+      transform: rotate(1deg);
+    }
+    25% {
+      transform: rotate(-1deg);
+    }
+    50% {
+      transform: rotate(1deg);
+    }
+    75% {
+      transform: rotate(-1deg);
+    }
+    100% {
+      transform: rotate(0deg);
+    }
+  }
+`;
+const IconPhone = () => {
+  return (
+    <Wrap>
+      <IMG>
+        <img width={"100%"} src={IMGphone} alt="" srcset="" />
+      </IMG>
+      
+      
+    </Wrap>
+  );
+};
+export default IconPhone;
