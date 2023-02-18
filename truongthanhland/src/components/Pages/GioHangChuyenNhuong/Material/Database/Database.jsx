8461b2ed4788721b2cc96454329d7f98f7cee857
vanhhoasen313 <ANH.DV0389@sinhvien.hoasen.edu.vn>
2023-02-18 12:05:29 +0700

    update

--- truongthanhland/src/components/Pages/GioHangChuyenNhuong/Material/Database/Database.jsx
+++ truongthanhland/src/components/Pages/GioHangChuyenNhuong/Material/Database/Database.jsx
@@ -1,17 +1,73 @@
 import IMGhouse1 from "./image/1.jpeg";
 import IMGhouse2 from "./image/2.jpeg";
+// V3-04
 import IMGV3041 from "./image/v3-04-1.jpg";
 import IMGV3042 from "./image/v3-04-2.jpg";
 import IMGV3043 from "./image/v3-04-3.jpg";
 import IMGV3044 from "./image/v3-04-4.jpg";
 import IMGV3045 from "./image/v3-04-5.jpg";
-
+// D2A022
 import IMGD2A021 from "./image/d2a-02-1.jpg";
 import IMGD2A022 from "./image/d2a-02-2.jpg";
 import IMGD2A023 from "./image/d2a-02-3.jpg";
 import IMGD2A024 from "./image/d2a-02-4.jpg";
 import IMGD2A025 from "./image/d2a-02-5.jpg";
+// V3-98
+import IMGV3981 from "./image/v3-98-1.jpg";
+import IMGV3982 from "./image/v3-98-2.jpg";
+import IMGV3983 from "./image/v3-98-3.jpg";
+import IMGV3984 from "./image/v3-98-4.jpg";
+import IMGV3985 from "./image/v3-98-5.jpg";
+import IMGV3986 from "./image/v3-98-6.png";
 
+// V3-267
+import IMGV32671 from "./image/v3-267-1.jpg";
+import IMGV32672 from "./image/v3-267-6.png";
+import IMGV32673 from "./image/v3-191-4.jpg";
+import IMGV32674 from "./image/v3-191-5.jpg";
+
+//V3245
+import IMGV32451 from "./image/v3-245-1.jpg";
+import IMGV32452 from "./image/v3-245-2.jpg";
+import IMGV32453 from "./image/v3-245-3.jpg";
+// import IMGV32454 from "./image/v3-245-4.jpg";
+import IMGV32455 from "./image/v3-245-5.jpg";
+import IMGV32456 from "./image/v3-245-6.jpg";
+import IMGV32457 from "./image/v3-245-7.png";
+//V3223
+import IMGV32231 from "./image/v3-223-1.jpg";
+import IMGV32232 from "./image/v3-223-2.jpg";
+import IMGV32233 from "./image/v3-223-3.jpg";
+import IMGV32234 from "./image/v3-223-4.jpg";
+import IMGV32235 from "./image/v3-223-5.jpg";
+import IMGV32236 from "./image/v3-223-6.jpg";
+//V3215
+
+import IMGV32151 from "./image/v3-215-1.jpg";
+import IMGV32152 from "./image/v3-215-2.jpg";
+import IMGV32153 from "./image/v3-215-3.jpg";
+import IMGV32154 from "./image/v3-215-4.jpg";
+import IMGV32155 from "./image/v3-215-5.jpg";
+import IMGV32156 from "./image/v3-215-6.jpg";
+
+//V3193
+import IMGV31931 from "./image/v3-193-1.jpg";
+import IMGV31932 from "./image/v3-191-2.jpg";
+import IMGV31933 from "./image/v3-191-3.jpg";
+import IMGV31934 from "./image/v3-191-4.jpg";
+import IMGV31935 from "./image/v3-191-5.jpg";
+import IMGV31936 from "./image/v3-193-6.jpg";
+
+//M241
+import IMGM2411 from "./image/m2-41-1.jpg";
+import IMGM2412 from "./image/m2-41-2.jpg";
+import IMGM2413 from "./image/m2-41-3.jpg";
+import IMGM2416 from "./image/m2-41-6.jpg";
+//T2019
+import IMGT20191 from "./image/t20-19-1.jpg";
+import IMGT20192 from "./image/t20-19-2.jpg";
+import IMGT20193 from "./image/t20-19-3.jpg";
+import IMGT20195 from "./image/t20-19-5.jpg";
 const Database = [
   {
     id: "V3-04",
@@ -64,34 +120,36 @@ const Database = [
     mucgia: 63,
   },
   {
-    id: "3",
+    id: "V3-98",
     tensanpham: "Biệt thự đơn lập V3-98",
-    image: [IMGhouse1, IMGhouse2],
-    diachi: "House 1",
+    image: [IMGV3981, IMGV3982, IMGV3983, IMGV3984, IMGV3985, IMGV3986],
+    diachi:
+      "Khu dân cư và công viên Phước Thiện thuộc phường Long Bình và Long Thạnh Mỹ",
     sotang: 3,
     phongngu: 4,
-    huongnha: "Đông Nam",
-    huongbancong: "Đông Nam",
+    huongnha: "Tây Nam",
+    huongbancong: "Tây Nam",
     trangthai: "Đã bàn giao",
     noithat: "bàn giao cơ bản",
     loaihang: "",
-    duongvao: "",
+    duongvao: "13",
     wc: 3,
-    mattien: "",
-    kichthuocsan: "",
-    dientich: 200,
+    mattien: "25",
+    kichthuocsan: "25 x 15",
+    dientich: 390,
     dacdiemnoibat: "",
     motavitri: "",
     congdongdancu: "",
     vegiaoduc: "",
     tienich: "",
-    mucgia: 20,
+    mucgia: 67,
   },
   {
-    id: "3",
+    id: "V3-267",
     tensanpham: "Biệt thự đơn lập V3-267",
-    image: [IMGhouse1, IMGhouse2],
-    diachi: "House 1",
+    image: [IMGV32671, IMGV32673, IMGV32674, IMGV32672],
+    diachi:
+      "Khu dân cư và công viên Phước Thiện thuộc phường Long Bình và Long Thạnh Mỹ.",
     sotang: 3,
     phongngu: 4,
     huongnha: "Đông Nam",
@@ -99,23 +157,24 @@ const Database = [
     trangthai: "Đã bàn giao",
     noithat: "bàn giao cơ bản",
     loaihang: "",
-    duongvao: "",
+    duongvao: "13",
     wc: 3,
-    mattien: "",
-    kichthuocsan: "",
-    dientich: 200,
+    mattien: "25",
+    kichthuocsan: "10 x 25",
+    dientich: 253,
     dacdiemnoibat: "",
     motavitri: "",
     congdongdancu: "",
     vegiaoduc: "",
     tienich: "",
-    mucgia: 20,
+    mucgia: 40,
   },
   {
-    id: "3",
-    tensanpham: "",
-    image: [IMGhouse1, IMGhouse2],
-    diachi: "House 1",
+    id: "V3-245",
+    tensanpham: "Biệt thự đơn lập V3-245",
+    image: [IMGV32451, IMGV32452, IMGV32453, IMGV32455, IMGV32456, IMGV32457],
+    diachi:
+      "Khu dân cư và công viên Phước Thiện thuộc phường Long Bình và Long Thạnh Mỹ.",
     sotang: 3,
     phongngu: 4,
     huongnha: "Đông Nam",
@@ -133,13 +192,14 @@ const Database = [
     congdongdancu: "",
     vegiaoduc: "",
     tienich: "",
-    mucgia: 20,
+    mucgia: 38,
   },
   {
-    id: "3",
-    tensanpham: "",
-    image: [IMGhouse1, IMGhouse2],
-    diachi: "House 1",
+    id: "V3-223",
+    tensanpham: "Biệt thự đơn lập V3-223",
+    image: [IMGV32231, IMGV32232, IMGV32233, IMGV32234, IMGV32235, IMGV32236],
+    diachi:
+      "Khu dân cư và công viên Phước Thiện thuộc phường Long Bình và Long Thạnh Mỹ",
     sotang: 3,
     phongngu: 4,
     huongnha: "Đông Nam",
@@ -147,17 +207,142 @@ const Database = [
     trangthai: "Đã bàn giao",
     noithat: "bàn giao cơ bản",
     loaihang: "",
-    duongvao: "",
+    duongvao: "13",
     wc: 3,
-    mattien: "",
-    kichthuocsan: "",
-    dientich: 200,
+    mattien: "10",
+    kichthuocsan: "10 x 25",
+    dientich: 321,
     dacdiemnoibat: "",
     motavitri: "",
     congdongdancu: "",
     vegiaoduc: "",
     tienich: "",
-    mucgia: 20,
+    mucgia: 53,
+  },
+  {
+    id: "V3-215",
+    tensanpham: "Biệt thự đơn lập V3-215",
+    diachi:
+      "Khu dân cư và công viên Phước Thiện thuộc phường Long Bình và Long Thạnh Mỹ",
+    dientich: 313,
+    mucgia: 65,
+    phongngu: 4,
+    wc: 3,
+    huongnha: "Tây Bắc",
+    huongbancong: "Tây Bắc",
+    noithat: "Bàn giao cơ bản",
+    trangthai: "Đã bàn giao",
+    sotang: 3,
+    mattien: "10",
+    duongvao: "13",
+    kichthuocsan: "10 x 31",
+    image: [IMGV32151, IMGV32152, IMGV32153, IMGV32154, IMGV32155, IMGV32156],
+    loaihang: "",
+    dacdiemnoibat: "",
+    motavitri: "",
+    congdongdancu: "",
+    vegiaoduc: "",
+    tienich: "",
+  },
+  {
+    id: "V3-193",
+    tensanpham: "Biệt thự đơn lập V3-193",
+    diachi:
+      "Khu dân cư và công viên Phước Thiện thuộc phường Long Bình và Long Thạnh Mỹ.",
+    dientich: 261,
+    mucgia: 60,
+    phongngu: 4,
+    wc: 3,
+    huongnha: "Tây Bắc",
+    huongbancong: "Tây Bắc",
+    noithat: "Bàn giao cơ bản",
+    trangthai: "Đã bàn giao",
+    sotang: 3,
+    mattien: "10",
+    duongvao: "13",
+    kichthuocsan: "10 x 26",
+    image: [IMGV31931, IMGV31932, IMGV31933, IMGV31934, IMGV31935, IMGV31936],
+    loaihang: "",
+    dacdiemnoibat: "",
+    motavitri: "",
+    congdongdancu: "",
+    vegiaoduc: "",
+    tienich: "",
+  },
+  {
+    id: "V3-191",
+    tensanpham: "Biệt thự đơn lập V3-191",
+    diachi:
+      "Khu dân cư và công viên Phước Thiện thuộc phường Long Bình và Long Thạnh Mỹ.",
+    dientich: 253,
+    mucgia: 60,
+    phongngu: 4,
+    wc: 3,
+    huongnha: "Tây Bắc",
+    huongbancong: "Tây Bắc",
+    noithat: "Bàn giao cơ bản",
+    trangthai: "Đã bàn giao",
+    sotang: 3,
+    mattien: "10",
+    duongvao: "13",
+    kichthuocsan: "10 x 25",
+    image: [IMGV31931, IMGV31932, IMGV31933, IMGV31934, IMGV31935, IMGV31936],
+    loaihang: "",
+    dacdiemnoibat: "",
+    motavitri: "",
+    congdongdancu: "",
+    vegiaoduc: "",
+    tienich: "",
+  },
+  {
+    id: "M2-41",
+    tensanpham: "Biệt thư đơn lập M2-41",
+    diachi:
+      "Khu dân cư và công viên Phước Thiện thuộc phường Long Bình và Long Thạnh Mỹ.",
+    dientich: 300,
+    mucgia: 45,
+    phongngu: 4,
+    wc: 3,
+    huongnha: "Tây Nam",
+    huongbancong: "Tây Nam",
+    noithat: "Bàn giao cơ bản",
+    trangthai: "Đã bàn giao",
+    sotang: 3,
+    mattien: "12",
+    duongvao: "13",
+    kichthuocsan: "25 x 12",
+    image: [IMGM2411, IMGM2412, IMGM2413, IMGM2416],
+    loaihang: "",
+    dacdiemnoibat: "",
+    motavitri: "",
+    congdongdancu: "",
+    vegiaoduc: "",
+    tienich: "",
+  },
+  {
+    id: "T20-19",
+    tensanpham: "Biệt thư song lập T20-19",
+    diachi:
+      "Khu dân cư và công viên Phước Thiện thuộc phường Long Bình và Long Thạnh Mỹ.",
+    dientich: 200,
+    mucgia: 30,
+    phongngu: 4,
+    wc: 3,
+    huongnha: "Tây Bắc",
+    huongbancong: "Tây Bắc",
+    noithat: "Bàn giao cơ bản",
+    trangthai: "Đã bàn giao",
+    sotang: 3,
+    mattien: "8",
+    duongvao: "13",
+    kichthuocsan: "8 x 25",
+    image: [IMGT20191, IMGT20192, IMGT20193, IMGT20195],
+    loaihang: "",
+    dacdiemnoibat: "",
+    motavitri: "",
+    congdongdancu: "",
+    vegiaoduc: "",
+    tienich: "",
   },
 ];
 
